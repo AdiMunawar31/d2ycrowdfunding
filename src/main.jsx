7d0443a76d3949e39686f535197e3d9c60603738
adimunawar31 <munawar.adi31@gmail.com>
2022-12-25 21:15:12 +0700

    implements react context

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import './index.css';
+import { StateContextProvider } from './context';
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <Router>
-        <App />
-      </Router>
-    </ThirdwebProvider>
+	<ThirdwebProvider desiredChainId={activeChainId}>
+		<Router>
+			<StateContextProvider>
+				<App />
+			</StateContextProvider>
+		</Router>
+	</ThirdwebProvider>
 );
